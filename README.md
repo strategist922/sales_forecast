502ef437ae0c31c04267473f4173f5f8d8be6ea4
AndisonoGe <AndisonoGe@users.noreply.github.com>
2017-10-07 08:58:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Sales Forecast by Product
+
+In the business world, sales forecasting is crucial. With good modeling and prediction, inventory cost is reduced and operation efficiency is improved.
+
+## Background
+
+### The Company
+
+This piece of code attempts to apply ARIMA model in sales prediction on a product-by-product basis. It has been used in a small medical device company based in American Midwest. The company has factory in China and the lead time is typically 1 - 2 months. If a product is out of stock, the company orders air shipment from China, which is expensive.
+
+### Products
+
+The products mainly include drug testing kits. Each product is a unique combination of drug testing strips that can test up to 16 different drugs. And there are over 100 different products. Some products have maintained three years of records. However, a small portion of products are newly marketed and have very short sales history (typically since 2016).
+
+In this publication, product codes are omitted for confidentiality.
+
+### The Model
+The available data include sales quantities of each product from Jan 2014 through May 2017. The last 5 months of records are reserved for model testing. Also, the data are pulled from QuickBooks Enterprise 2016, with time ("yyyy-mm") on column and product on row, which needs a transpose upon initiation.
+
+After some exploratory data analyses (EDA), ARIMA is selected. It is the ideal algorithm for time series modeling and predicting. A simple stationarity check rules out new products because they typically have high order of difference. This portion of products are submitted for human determination.
+
+After successfully fitted on the 36-month sales data, the model is tested using the sales data generated in the first 5 months of 2017. And finally, the model is used to predict sales for each existing (as opposed to new) products for the next 6 months and on a rolling basis (with modifications).
+
+## Prerequisites
+
+### Software
+
+* macOS Sierra
+* Python 3.6.2
+* IPython 5.3.0
+* Modules including:
+  - os
+  - datetime
+  - numpy
+  - pandas
+  - matplotlib
+  - statsmodels
+  - sqlalchemy
+
+### Running
+
+1. Navigate to the project folder and start Terminal from the folder. Otherwise, use ```cd``` command to navigate here.
+
+2. Copy and paste code chunks as needed. Be sure to modify according to specific environment and tasks.
+
+3. Limbos are stored under corresponding folders. 
+
+## Limitations
+
+### Too few records
+
+In practice, 3 years of records strike a balance between too few observations and too few available products. If we chose, say 10 years of records, most products will be ruled out as "new" products and subject to human determination. However, in the age of big data, 36 months still seem too short.
+
+But what if we pulled data in the period of week? That amounts to 52 * 3 = 156 weeks of records. The problem is that sales are not as smooth as those of online retailers for a B2B business. It makes no sense dissecting sales history into weeks.
+
+### Incomplete automation with new products determined manually
+
+In the future, the business will create complicated models that deal with products with short history. But for now, some hunch of and insight into the market make human irreplaceable. In the movie *Star Trek IV: The Voyage Home*, Captain Kirk asks Mr Spock to guess a parameter for their return to the future, which is a torture for Mr Spock, who is the embodiment of pure logic.
+
+### Sales prediction vs. production plan
+
+This prediction or forecasting is in any sense only a draft for how many of each product will be ordered in the future. Taking into consideration of lead time and shipping costs, the result of this piece of code is hardly the final version of production plan. Some specialized models should also be considered. For example, EOQ model accounts for holding costs and other factors, which is ideal in determining the timetable for production, from procurement of raw materials to hiring production workers and so on. Also, predictions of product sales must be decomposed to individual drug tested. In essence, the company manufactures test strips, not plastic containers.
+
+## Updates
+
+* 10/06/2017 Initial release after confidentiality check.
+
+## License
+ GPL3
---
+++ ana.py
@@ -0,0 +1,234 @@
+import os
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller as ADF
+from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.api as sm
+from statsmodels.graphics.api import qqplot
+from statsmodels.stats.diagnostic import acorr_ljungbox
+
+# data prep
+fin = 'data/sales_by_item.xlsx'
+out_path = 'result/'
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+fout = 'result/forecast.xlsx'
+mout = 'result/model.xlsx'
+
+dt = pd.read_excel(fin).T # T b/c originally date as column and product as row
+dt.index = pd.to_datetime(dt.index)
+dt_ana = dt.iloc[:-12, :] # use 2014-2016 data for modeling
+dt_tst = dt.iloc[-12:-7, :] # use 2017.01-05 data for testing
+dt_prd = dt.iloc[-7: -1, :] # predict 2017.06-11 data
+
+######################################################################
+# EDA
+figout = 'eda_fig/'
+if not os.path.exists(figout):
+    os.makedirs(figout)
+
+plt.style.use('ggplot')
+for column in dt_ana.columns:
+    colout = os.path.join(figout, column + '.png')
+    dt_ana[column].plot(figsize=(10,8))
+    plt.title(column)
+    plt.xlabel('Date')
+    plt.ylabel('Sales Quantity')
+    plt.savefig(colout, dpi=200)
+    plt.close()
+
+######################################################################
+# Adfuller test to determine order of diff and potentially opt out new products
+diff = dict() # store diff info for every product
+p_adf = dict() # store p-value for every product corresponding to diff value
+
+for column in dt_ana.columns:
+    adf = ADF(dt_ana[column])
+    diff[column] = 0
+    p_adf[column] = adf[1]
+    while adf[1] >= .05:
+        try:
+            diff[column] = diff[column] + 1
+            adf = ADF(dt_ana[column].diff(diff[column]).dropna())
+            p_adf[column] = adf[1]
+        except:
+            diff[column] = -1
+
+# check the relationship for screening invalid products
+[(x, diff[x], p_adf[x]) for x in dt.columns] # command line
+# new products have too short records and will be submitted for human decision
+new_prod = [k for (k, v) in p_adf.items() if np.isnan(v) or v == 0.0]
+# current products are submitted for modeling
+cur_prod = [x for x in dt.columns if x not in new_prod]
+
+######################################################################
+# plot ACF and PACF
+acf_pacf_out = 'acf_pacf_fig/'
+if not os.path.exists(acf_pacf_out):
+    os.makedirs(acf_pacf_out)
+
+def plot_acf_pacf(data, savefig=False, savename=''):
+    lag = len(data) - 1
+    fig = plt.figure(figsize=(10,8))
+    ax1 = fig.add_subplot(211)
+    fig = sm.graphics.tsa.plot_acf(data,lags=lag, ax=ax1)
+    ax2 = fig.add_subplot(212)
+    fig = sm.graphics.tsa.plot_pacf(data,lags=lag, ax=ax2)
+    if not savefig:
+        plt.show()
+    else:
+        plt.savefig(savename, dpi=200)
+
+# loop through all current products
+for column in cur_prod:
+    # print('Working on %s' %column)
+    d = diff[column]
+    if d == 0:
+        data = dt_ana[column]
+    else:
+        data = dt_ana[column].diff(d).dropna()
+    save_dir = os.path.join(acf_pacf_out, column + '(diff ' + str(d) + ').png')
+    plot_acf_pacf(data, True, save_dir)
+    plt.close()
+
+######################################################################
+# calculate BIC to determine p and q
+l = int(len(cur_prod) / 10)
+pmax = l
+qmax = l
+bic = dict()
+
+# loop through all current products
+for column in cur_prod:
+    print('Working on %s' %column)
+    d = diff[column]
+    data = dt_ana[column].astype(float) #
+    bic_matrix = []
+    for p in range(pmax + 1):
+        tmp = []
+        for q in range(qmax + 1):
+            try:
+                tmp.append(ARIMA(data, (p, d, q)).fit().bic)
+            except:
+                tmp.append(np.nan)
+        bic_matrix.append(tmp)
+    bic[column] = bic_matrix
+    print('-' * 80)
+
+# products with empty BIC series are submitted for manual determination of p & q
+manual_check = []
+
+for column in cur_prod:
+    data = pd.DataFrame(bic[column])
+    if int(data.isnull().sum().sum()) == (l + 1) ** 2:
+        print('%s is empty and should be manually checked. ' %column)
+        manual_check.append(column)
+
+machine_run = [x for x in cur_prod if x not in manual_check]
+
+# retrieve p and q
+p_q = dict()
+
+for column in machine_run:
+    data = pd.DataFrame(bic[column])
+    p, q = data.stack().idxmin()
+    p_q[column] = [p, q]
+
+# store p, d, q for all available items
+params = pd.DataFrame()
+for column in machine_run:
+    p = p_q[column][0]
+    q = p_q[column][1]
+    d = diff[column]
+    params[column] = pd.Series([p, d, q])
+
+######################################################################
+# test the model(s)
+lag = 6
+validity = dict()
+model = dict()
+
+for column in machine_run:
+    try:
+        # predict
+        arima = ARIMA(dt_ana[column].astype(float), tuple(params[column])).fit()
+        predicted = arima.predict()
+        #measure
+        error = pd.Series(predicted) - dt_ana[column]
+        lb, p = acorr_ljungbox(error, lags=1)
+        h = (p < 0.05).sum() # valid model has all p > 0.05, i.e. h == 0
+        if h > 0:
+            validity[column] = False
+            manual_check.append(column)
+        else:
+            validity[column] = True
+            model[column] = arima
+    except:
+        # if it fails to predict at all, it is invalid
+        validity[column] = False
+        manual_check.append(column)
+
+machine_run = [x for x in cur_prod if x not in manual_check]
+
+params[machine_run].to_excel(mout)
+
+######################################################################
+# try out the models
+forecast = dict()
+start = pd.Timestamp(np.datetime64('2017-01'))
+end = pd.Timestamp(np.datetime64('2017-12'))
+
+#test
+result = pd.concat([dt_ana[cc], amd.predict(start, end)])
+ori = dt_ana[cc]
+prd = amd.predict(start, end)
+
+for column in machine_run:
+    # predict
+    ori = dt_ana[column]
+    prt = dt_tst[column]
+    prd = amd.predict(start, end)
+
+    # plot
+    plt.figure(figsize=(10,5))
+    plt.title('Sales Projection of %s for %s - %s' %(cc, str(start)[:7], str(end)[:7]))
+    plt_ori = plt.plot(ori, color='r', label='History', linestyle='-')
+    plt_ori = plt.plot(prt, color='c', label='YTD', linestyle='-')
+    plt_prd = plt.plot(prd, color='g', label='Projection', linestyle='--')
+    plt.xlabel('Date')
+    plt.ylabel('Sales Quantity')
+    plt.legend(loc=0)
+
+    # save
+    fig_name = column + ' for ' + str(start)[:7] + ' - ' + str(end)[:7] + '.png'
+    fig_path = os.path.join(out_path, fig_name)
+    plt.savefig(fig_path, dpi=200)
+
+    #close plot
+    plt.close()
+
+
+# Submit for supervisory inspection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
